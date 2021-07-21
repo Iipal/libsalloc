95dde2b01f72b7a3d03f3648b7f36c3c0f5b8e7e
Iipal <malkoleyplay@gmail.com>
2021-07-22 00:51:50 +0300

    removed tests from main branch

--- test/gdi_simple.c
+++
@@ -1,9 +0,0 @@
-#define SALLOC_GDI_BUFFER
-#include "../salloc.h"
-
-int main(void) {
-  const salloc_size_t chunk_size = 16;
-  salloc_ptr_t        chunk      = salloc(chunk_size);
-
-  sfree_gdi(chunk);
-}
--- test/vla.c
+++
@@ -1,5 +0,0 @@
-#include "../salloc.h"
-
-const salloc_size_t vla = 32;
-
-int main(void) { salloc_new_fast(, vla); }
